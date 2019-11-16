6c0718ea24bc4440d6e484601aaac2d8dabd75e8
Michael Slattery <mike@slattery.com>
2019-11-15 20:27:02 -0500

    reorg

--- README.md
+++ README.md
@@ -8,4 +8,22 @@ The goal is a program or script that can install Linux on an existing Windows wo
 
 This project has just started and is simply a proof-of-concept.  It is of low quality.
 Do not run any of the included scripts directly.
-It would be better to copy paste them line-by-line and google each command to understand what it does.
+It wouldbe better to copy paste them line-by-line and google each command to understand what it does.
+
+## Getting Started
+
+### Installation
+
+Simply clone this repository with git.
+
+### Usage
+
+All scripts are meant to be run from the PowerShell terminal.
+
+`.\clean.ps1` - Remove various files to reduce disk usage, similar to CCleaner.
+
+`.\repartition.\ps1` - Shrink the Windows partition and use the remaining space for new Linux partition(s).
+
+`.\install-vm.ps1` - Install Linux on the new partition(s) and add to boot menu.
+
+`.\unclean.ps1` - Add important files remvoed by `clean.ps1`, such as swap and hibernation files.
--- install-vm.ps1
+++ install-vm.ps1
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-set -euo pipefail
 
 USER="$(whoami)"
 name="Mint2"
@@ -8,25 +5,25 @@ password="mike68"
 tzone='EST'
 country='US'
 hostname='hostname'
-rawdisk='\\.\PHYSICALDRIVE0'
+rawdisk='`\.\PHYSICALDRIVE0'
 
-vboxmanage createvm -name "$name" \
-  --ostype Ubuntu_64 \
+vboxmanage createvm -name "$name" `
+  --ostype Ubuntu_64 `
   --register
 
-vboxmanage modifyvm "$name" \
-  --cpus 1 \
-  --memory 1024 \
+vboxmanage modifyvm "$name" `
+  --cpus 1 `
+  --memory 1024 `
   --vram 12
 
-VBoxManage internalcommands createrawvmdk \
-  -filename 'C:\linux\15.vmdk' \
+VBoxManage internalcommands createrawvmdk `
+  -filename 'C:`linux\15.vmdk' \
   -rawdisk "$rawdisk" -partitions 1,5
 
-VBoxManage storageattach "$name" \
-  --storagectl "IDE Controller" \
-  --port 0 --device 0 --type hdd \
-  --medium 'C:\linux\15.vmdk'
+VBoxManage storageattach "$name" `
+  --storagectl "IDE Controller" `
+  --port 0 --device 0 --type hdd `
+  --medium 'C:`linux\15.vmdk'
 
 #TODO: mount iso as .vdi, launch vm w/gui
 #TODO: unattended w/o harming efi
@@ -37,12 +34,12 @@ VBoxManage storageattach "$name" \
 # [--auxiliary-base-path=path] [--image-index=number] [--script-template=file] [--post-install-template=file]
 # [--post-install-command=command] [--extra-install-kernel-parameters=params] [--language=lang]
  
-vboxmanage unattended install "$name" \
-  --user "$USER" --password="$password" \
-  --install-additions \
-  --time-zone "$tzone" \
-  --hostname "$hostname" \
-  --country "$country" \
-  --iso 'C:\linux\mintcin.iso' \
+vboxmanage unattended install "$name" `
+  --user "$USER" --password="$password" `
+  --install-additions `
+  --time-zone "$tzone" `
+  --hostname "$hostname" `
+  --country "$country" `
+  --iso 'C:`linux\mintcin.iso' \
   --start-vm gui
 
